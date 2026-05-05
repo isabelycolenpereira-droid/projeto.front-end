f1ba8b1968bf4ae4fa9e0114da5e96d0a96fa6e1
isabelycolenpereira-droid <isabely.colen.pereira@escola.pr.gov.br>
2026-05-05 11:20:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-let frases = ["F1", "F2", "F3"];
-let frasesCopiada = [...frases];
+let frases=["Os ecos do céu são as respostas que buscamos, sussurrando esperança em cada batida do nosso coração.", "Quando ouvimos os ecos do céu, encontramos a paz que buscamos na Terra.","Que o eco do céu nos conduza, nos fortaleça e nos inspire a viver com propósito e gratidão"];
+    
+    function geradorMensagem(){
+    let indiceGerado= Math.floor(Math.random()*(frases.length));
+    let fraseGerada= frases[indiceGerado];
+    document.querySelector("#saída").textContent=fraseGerada
 
-
-function geradorMensagem(){
-if(frases.length == 0){
-frases = [...frasesCopiada];
-}
-let indiceGerado = Math.floor(Math.random()*(frases.length));
-let fraseGerada = frases [indiceGerado];
-console.log(fraseGerada);
-document.querySelector("#msg").textContent = fraseGerada;
-frases.splice(indiceGerado,1);
-
-}
+    }
